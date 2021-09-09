01737c312bdd28ee8d5fde7478b7d911980ae028
Anisha-asweety <70945347+Anisha-asweety@users.noreply.github.com>
2021-09-09 21:17:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,27 +2,30 @@ var hr,min,sec
 
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-   
+  angleMode(DEGREES)
+
+}
+
+function draw() {
+  background(0,0,0);
+  translate(200,200)
+  rotate(-90)
+
   //Calculating time using predifined func from p5.js
   hr=hour();
   min=minute();
   sec=second();
-}
-
-function draw() {
-  background(255,255,255);
   
-  hrAngle=map(hr,0,100,0,360)
+  hrAngle=map(hr%12,0,12,0,360)
   secAngle=map(sec,0,60,0,360)
-  angleMode(DEGREES)
+  minAngle=map(min,0,60,0,360)
   
    //drawing hours hand
    push();
    rotate(hrAngle);//rotate the hand based on angle calculated
    stroke(0,255,0);
    strokeWeight(7);
-   line(100,100,50,0);
+   line(0,0,50,0);
    pop();
  
   //drawing seconds hand
@@ -30,7 +33,15 @@ function draw() {
   rotate(secAngle);//rotate the hand based on angle calculated
   stroke(255,0,0);
   strokeWeight(7);
-  line(400,0,75,0);
+  line(0,0,100,0);
+  pop();
+
+  //drawing minutes hand
+  push();
+  rotate(minAngle);//rotate the hand based on angle calculated
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,75,0);
   pop();
 
   drawSprites();
